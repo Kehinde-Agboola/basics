a9ebc11d4ce5ff7e55d2352d32f9ed95026b5b39
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-01-19 08:13:33 +0100

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // title = 'basics';
   imgURL = 'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0'
-  name = 'Kehinde';
-  getName(){
-    return this.name
+  name = 'kehinde';
+  images = [
+    'https://picsum.photos/seed/picsum/200/300',
+    'https://picsum.photos/200/300/?blur',
+    'https://picsum.photos/id/870/200/300?grayscale&blur=2'
+  ]
+  currentDate = new Date()
+  cost = 2000
+  temp = 25.3
+  pizza = {
+    toppings:['Garri', 'Beans', 'Dodo']
   }
+  blueClass = false
+  // greenClass = 
+  fontSize = 16
+
+  // currentDate = 
+
   changeImage(e: KeyboardEvent){
     this.imgURL = (e.target as HTMLInputElement).value
   }
